7b0a6032952c84e6dc32a106c94c79732b5bd234
Phil <p.vancaloen@hotmail.com>
2019-07-03 06:11:04 +0200

    front

--- src/components/Step3/index.js
+++ src/components/Step3/index.js
@@ -3,11 +3,33 @@ import { Button } from 'reactstrap';
 import {connect} from 'react-redux';
 
 class Step3 extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: ""
+    }
+  }
   handleClick = () => {
-    const url = ''
+    const url = 'http://localhost:5000/playlist/92221047';
+    const config = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: {
+        "playlist_name": "playlist1",
+        "tracks": ["chanson1", "chanson2"]
+      },
+  }
+
+  fetch(url, config)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({id: data.data.confirmationNo})
+    })
   }
     render(){
-      const {playlist_name, track1, track2} = this.props.data
+      const {playlist_name, track1, track2 } = this.props.data
       console.log(this.props.data)
         return(
           <div>
@@ -17,6 +39,7 @@ class Step3 extends React.Component {
               <li>{track2}</li>
             </ul>
             <Button onClick={this.handleClick} size="lg" color="secondary">Confirm</Button>
+            <h3>{this.state.id}</h3>
           </div>
         )
     }
